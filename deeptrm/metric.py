3cc5bb70e733eddcbbdcd8e8dd64d1dfdf4bba0c
Rorschach1989 <evanescentencounted@gmail.com>
2022-03-28 23:42:09 +0800

    Add memory-efficient c_index

--- deeptrm/metric.py
+++ deeptrm/metric.py
@@ -1,4 +1,5 @@
 import torch
+import numba
 
 
 def c_index(y_pred, y_true, delta):
@@ -14,3 +15,25 @@ def c_index(y_pred, y_true, delta):
     c_pred = (m1 * m2 * delta).sum()
     c_true = (m2 * delta).sum()
     return c_pred / c_true
+
+
+@numba.njit(parallel=True)
+def c_index_large_scale(y_pred, y_true, delta):
+    """Acceleration of conventional c-index via JIT
+
+    Args:
+        y_pred(np.ndarray): rank-1 array of shape [batch_size,]
+        y_true(np.ndarray): rank-1 array of shape [batch_size,]
+        delta(np.ndarray): rank-1 array of shape [batch_size,]
+    """
+    nom = 0.
+    denom = 0.
+    n = y_pred.shape[0]
+    for i in numba.prange(n):
+        for j in numba.prange(n):
+            m1_ij = numba.float32(y_pred[i] < y_pred[j])
+            m2_ij = numba.float32(y_true[i] < y_true[j])
+            nom = nom + m1_ij * m2_ij * delta[i]
+            denom = denom + m2_ij * delta[i]
+    return nom / denom
+
