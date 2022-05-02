2b3990d0928b189ae6a48abc55bb422e35b6fb89
Rorschach1989 <evanescentencounted@gmail.com>
2022-05-02 13:28:19 +0800

    Update CoxPH results

---
+++ experiment_kkbox_ph.py
@@ -0,0 +1,58 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from torch.utils.data import DataLoader
+from deeptrm.utils import default_device
+from deeptrm.datasets import SurvivalDataset
+from deeptrm.base import TransNLL, MonotoneNLL
+from deeptrm.eps_config import GaussianEps, CoxEps, ParetoEps, NonparametricEps, BoxCoxEps, PositiveStableEps
+from pycox.evaluation.eval_surv import EvalSurv
+from sksurv.linear_model import CoxPHSurvivalAnalysis
+
+
+kkbox_train = SurvivalDataset.kkbox('train')
+kkbox_valid = SurvivalDataset.kkbox('val')
+kkbox_test = SurvivalDataset.kkbox('test')
+valid_c_indices, test_c_indices = [], []
+valid_ibs, test_ibs = [], []
+valid_inbll, test_inbll = [], []
+y_train, delta_train, z_train = kkbox_train.sort()
+y_valid, delta_valid, z_valid = kkbox_valid.sort()
+y_test, delta_test, z_test = kkbox_test.sort()
+
+
+with torch.no_grad():
+    y_train = y_train.numpy()
+    delta_train = delta_train.numpy()
+    y_test = y_test.numpy()
+    delta_test = delta_test.numpy()
+
+
+train_tuple = np.array(
+    [(bool(_d), _y) for _y, _d in zip(y_train.reshape(-1), delta_train.reshape(-1))],
+    dtype=[('e.tdm', '?'), ('t.tdm', '<f8')]
+)
+rsf = CoxPHSurvivalAnalysis(alpha=1e-2)
+rsf.fit(z_train, train_tuple)
+survs = rsf.predict_survival_function(z_test)
+y_min, y_max = y_train.min(), y_train.max()
+tg_test = np.linspace(  # Overflow is not allowed in sksurv
+    max(y_min, y_test.min()), min(y_max, y_test.max()), 100)
+# tg_test = np.linspace(y_test.min(), y_test.max(), 100)
+preds = np.asarray([[fn(t) for t in tg_test] for fn in tqdm(survs)])
+test_evaluator = EvalSurv(
+    surv=pd.DataFrame(preds.T, index=tg_test.reshape(-1)),
+    durations=y_test.reshape(-1),
+    events=delta_test.reshape(-1),
+    censor_surv='km')
+test_tuple = np.array(
+    [(bool(_d), _y) for _y, _d in zip(np.clip(y_test, y_min, y_max).reshape(-1), delta_test.reshape(-1))],
+    # [(bool(_d), _y) for _y, _d in zip(y_test.reshape(-1), delta_test.reshape(-1))],
+    dtype=[('e.tdm', '?'), ('t.tdm', '<f8')]
+)
+print(test_evaluator.concordance_td(method='antolini'))
+print(test_evaluator.integrated_brier_score(time_grid=tg_test))
+print(test_evaluator.integrated_nbll(time_grid=tg_test))
+
